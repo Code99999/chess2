f488670220a093e0ada55ffd127a35270c0796aa
Code99999 <74378746+Code99999@users.noreply.github.com>
2025-04-26 10:33:48 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
@@ -7,15 +6,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Serve the index.html file
-app.use(express.static(__dirname));
+app.use(express.static(__dirname)); // Serve the index.html
 
 io.on('connection', (socket) => {
-    socket.on('move', (data) => socket.broadcast.emit('move', data));
-    socket.on('timeout', () => socket.broadcast.emit('turn', null));
-    socket.on('chat', (msg) => io.emit('chat', msg));
-    socket.on('name', (name) => io.emit('name', name));
-    socket.on('restart', () => io.emit('restart'));
+  socket.on('move', (data) => socket.broadcast.emit('move', data));
+  socket.on('timeout', () => socket.broadcast.emit('turn', null));
+  socket.on('chat', (msg) => io.emit('chat', msg));
+  socket.on('name', (name) => io.emit('name', name));
+  socket.on('restart', () => io.emit('restart'));
 });
 
-server.listen(3000, () => console.log('Server running at http://localhost:3000'));
+server.listen(process.env.PORT || 3000, () => console.log('Server running!'));
